418545a4468d018442860b57a1a2eb4b9562db91
knuchanwoo <pove2019@gmail.com>
2021-11-14 02:37:56 +0900

    feat: add CarouselSlider
    Added carousel image
    Added text to indicate genre and 3 icons
    Added the makeIndicator method indicating the current page

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_project/model/model_movie.dart';
 
@@ -9,10 +10,10 @@ class CarouselImage extends StatefulWidget {
 }
 
 class _CarouselImageState extends State<CarouselImage> {
-  late List<Movie> movies;
-  late List<Widget> images;
-  late List<String> keywords;
-  late List<bool> likes;
+  late List<Movie> movies = [];
+  late List<Widget> images = [];
+  late List<String> keywords = [];
+  late List<bool> likes = [];
   late int _currentPage = 0;
   late String _currentKeyword;
   @override
@@ -22,10 +23,121 @@ class _CarouselImageState extends State<CarouselImage> {
     images = movies.map((m) => Image.asset('./images/' + m.poster)).toList();
     keywords = movies.map((m) => m.keyword).toList();
     _currentKeyword = keywords[0];
+    likes = movies.map((m) => m.like).toList();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+          ),
+          CarouselSlider(
+            items: images,
+            options: CarouselOptions(onPageChanged: (index, reason) {
+              setState(() {
+                _currentPage = index;
+                _currentKeyword = keywords[_currentPage];
+              });
+            }),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(
+              _currentKeyword,
+              style: TextStyle(fontSize: 11),
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      likes[_currentPage]
+                          ? IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.check),
+                            )
+                          : IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.add),
+                            ),
+                      Text(
+                        '내가 찜한 콘텐츠',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: FlatButton(
+                    color: Colors.white,
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.black,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(3),
+                        ),
+                        Text(
+                          '재생',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Column(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.info),
+                      ),
+                      Text(
+                        '정보',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: makeIndicator(likes, _currentPage),
+          ))
+        ],
+      ),
+    );
   }
+}
+
+List<Widget> makeIndicator(List list, int _currentPage) {
+  List<Widget> results = [];
+  for (var i = 0; i < list.length; i++) {
+    results.add(Container(
+      width: 8,
+      height: 8,
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: _currentPage == i
+            ? Color.fromRGBO(255, 255, 255, 0.9)
+            : Color.fromRGBO(255, 255, 255, 0.4),
+      ),
+    ));
+  }
+  return results;
 }
